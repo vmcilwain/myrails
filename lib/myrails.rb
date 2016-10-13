6b630fbc29a30de9ffe49e9faffac84ff4f9cd3c
Vell <lovell.m@teachingstrategies.com>
2016-10-13 11:29:44 -0400

    bump to 1.0.2

--- lib/myrails.rb
+++ lib/myrails.rb
@@ -22,9 +22,9 @@ module Myrails
     desc 'controller --name=controller-name', 'Generate a rails controller with the given name along with boiler plate code and related spec filet'
     option :name, required: true
     def controller
-      template 'rails/controller.rb', "app/controllers/#{options[:name]}_controller.rb"
-      template 'rspec/controller.rb', "spec/controllers/#{options[:name]}_controller_spec.rb"
-      run "mkdir app/views/#{options[:name]}"
+      template 'rails/controller.rb', "app/controllers/#{options[:name].pluralize}_controller.rb"
+      template 'rspec/controller.rb', "spec/controllers/#{options[:name].pluralize}_controller_spec.rb"
+      run "mkdir -p app/views/#{options[:name].pluralize}"
     end
 
     desc 'policy --name=policy-name', 'Generate a pundit policy with the given name and a related spec file'

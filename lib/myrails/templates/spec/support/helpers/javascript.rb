8160eb083ca118e864d69e3407ed566bf6e9c300
Vell <vmcilwain@me.com>
2017-12-20 14:17:51 +0000

    update rspec javascript helper

--- lib/myrails/templates/spec/support/helpers/javascript.rb
+++ lib/myrails/templates/spec/support/helpers/javascript.rb
@@ -1,8 +1,12 @@
 module JavascriptHelper
   # Basecamp trix uses hidden input to populate its editor
   def fill_in_trix_editor(id, value)
-    find(:xpath, "//*[@id='#{id}']", visible: false).set(value)
+    find(:css, id).set(value)
+  end
+
+  def click_ok
+    page.accept_alert
   end
 end
 
-# Example of use in feature test: fill_in_trix_editor("blog_comment_body_trix_input_blog_comment_#{blog_comment.id}", "some text")
+# Example of use in feature test: fill_in_trix_editor("#blog_comment_body_trix_input_blog_comment_#{blog_comment.id}", "some text")

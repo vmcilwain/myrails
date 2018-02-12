38f6c018eff1911dde62e3526af2cf173115d1ce
Vell - [I am available to join projects.] <vmcilwain@me.com>
2018-02-12 09:38:04 -0500

    updates to pundit install

--- lib/myrails/modules/pundit.rb
+++ lib/myrails/modules/pundit.rb
@@ -5,25 +5,21 @@ module Install
 
         def add_pundit_gems
           insert_into_file 'Gemfile', after: "gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]\n" do <<-CODE
-  gem 'pundit'
-          CODE
+gem 'pundit'
+CODE
           end
 
           insert_into_file 'Gemfile', after: "group :test do\n" do <<-CODE
-    gem 'pundit-matchers', '~> 1.1.0'
-  CODE
+  gem 'pundit-matchers'
+CODE
           end
           run 'bundle update'
         end
 
         def enable_pundit
-          inject_into_file 'app/controllers/application_controller.rb', after: "protect_from_forgery with: :exception\n" do <<-CODE
+          inject_into_file 'app/controllers/application_controller.rb', after: "class ApplicationController < ActionController::Base\n" do <<-CODE
   # Add pundit authorization
   include Pundit
-CODE
-          end
-
-          inject_into_file 'app/controllers/application_controller.rb', after: "rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized\n" do <<-CODE
   # Rescue from pundit error
   # (see #user_not_authorized)
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
@@ -36,7 +32,7 @@ CODE
   # @return flash [Hash] the action notice
   def user_not_authorized
     flash[:alert] = "You are not authorized to perform this action."
-    redirect_to home_path
+    redirect_to root_path
   end
 CODE
           end
